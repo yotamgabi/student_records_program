f139fef08f40876b97fccae402a1ad423c67db06
Yotam <yotga1@gmail.com>
2022-05-09 17:17:32 +0300

    normal procedure

--- classes/Student.py
+++ classes/Student.py
@@ -1,20 +1,11 @@
 from classes.Course import Course
 
 
-def new_course(course_name: str):
-    return Course(course_name)
-
-
 class Student:
-    _class = {}  # map student id to objects
-    _trash = {}  # map student id to objects
-
     def __init__(self, student_id: int, name: str, age: int):
         self._id = student_id
         self._name = name
         self._age = age
-        self._courses = {}
-        self._class[student_id] = self
 
     @property
     def id(self):
@@ -32,22 +23,3 @@ class Student:
     def courses(self):
         return self._courses
 
-    @property
-    def grades(self):
-        grades = {}
-        for course_obj in self._courses.keys():
-            if course_obj in
-
-
-    def sign_to_course(self, course_obj: Course):
-        self._courses[course_obj] = None  # course OBJECT -> @todo: decide what to map to
-
-    def set_grade(self, course_obj: Course, grade: float):
-        course_obj.set_grade(self, grade)
-
-    def remove(self):
-        del self._courses[self.id]
-        self._trash[self.id] = self
-
-        @todo
-        # remove student grades from all courses
